22239d1ea2c0855e6373562f31ce5d0f7ff9bfe4
Colin Casey <casey.colin@gmail.com>
2013-03-11 17:59:54 -0300

    added flat-ui css framework

--- config/application.rb
+++ config/application.rb
@@ -55,6 +55,7 @@ module Moleskin
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'flat-ui')
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
